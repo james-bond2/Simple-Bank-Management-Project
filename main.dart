ce669cce491f09a6c4a9f2ee1d9981192229c689
Imran Hossain <asa691317@gmail.com>
2024-09-29 21:03:34 +0600

    corrected
    second commit

--- main.dart
+++ main.dart
@@ -1,6 +1,6 @@
 void main(){
-  Account nasim = Account(user: 'nasim', balance: 5000, userAccount: 'abc246');
-  Account abdullah = Account(user: 'abdullah', balance: 6788, userAccount: '456rfv2345');
+  Account nasim = Account(user: 'nasim', userAccount: 'abc246');
+  Account abdullah = Account(user: 'abdullah',  userAccount: '456rfv2345');
   nasim.balanceDeposite(9500);
   nasim.displayAccount();
   nasim.balanceWithdraw(7654);
@@ -14,29 +14,35 @@ void main(){
 class Account{
   String user = '';
   String userAccount = '';
-  double balance =0;
-  static int accounts = 0;
-  Account({required this.user, required this.balance, required this.userAccount}){}
-  int totalAccounts(){
-    return accounts;
+  double _balance =500;
+  static int _accounts = 0;
+  Account({required this.user, required this.userAccount}){
+    _accounts ++;
+   }
+getAccounts() {
+  if (_accounts == 1) {
+    print('There is $_accounts account.');
+  } else {
+    print('There are $_accounts accounts.');
   }
+}
    void balanceDeposite(double amount){
      if(amount > 0){
-       balance = balance + amount;
-       print('New balance is $balance.');
+       _balance = _balance + amount;
+       print('New balance is $_balance.');
      }else{
        print('Please put a positive value.');
      }
    }
    void balanceWithdraw(double amount){
-     if(amount <= balance && amount >0){
-       balance = balance - amount;
-       print('New balance is $balance.');
+     if(amount <= _balance && amount >0){
+       _balance = _balance - amount;
+       print('New balance is $_balance.');
      }else{
        print('Please put a valid amount');
      }
    }
     void displayAccount(){
-      print("User Name: $user, Total Balance: $balance");
+      print("User Name: $user, Total Balance: $_balance");
     }
 }
